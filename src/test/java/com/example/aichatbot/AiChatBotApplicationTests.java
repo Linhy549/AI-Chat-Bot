fe7ceb6c26d9c0e9884f220b405a9c707d1971c8
Linhy549 <1098313754@qq.com>
2025-07-14 16:05:59 -0400

    First commit

---
+++ src/test/java/com/example/aichatbot/AiChatBotApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.aichatbot;
+
+import com.example.aichatbot.aiHelper.AiHelper;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiChatBotApplicationTests {
+
+    @Resource
+    private AiHelper aiHelper;
+    @Test
+    void chat() {
+        aiHelper.chat("Hey ChatGPT!");
+    }
+
+}
