182d5a15a91ac1d8f0d1eb7b8cf5ed611d7ca705
Linhy549 <1098313754@qq.com>
2025-09-26 15:17:15 -0400

    Added AiHelperServiceFactory bean

--- src/main/java/com/example/aichatbot/aiHelper/AiHelperServiceFactory.java
+++ src/main/java/com/example/aichatbot/aiHelper/AiHelperServiceFactory.java
@@ -1,10 +1,15 @@
 package com.example.aichatbot.aiHelper;
 
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.StreamingChatModel;
 import dev.langchain4j.service.AiServices;
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import dev.langchain4j.rag.content.retriever.ContentRetriever;
+
 
 @Configuration
 public class AiHelperServiceFactory {
@@ -14,8 +19,22 @@ public class AiHelperServiceFactory {
     @Resource
     private ChatModel ollamaChatModel;
 
+    @Resource
+    private StreamingChatModel ollamaStreamingChatModel;
+
     @Bean
     public AiHelperService aiHelperService(){
-        return AiServices.create(AiHelperService.class, ollamaChatModel);
+
+//        return AiServices.create(AiHelperService.class, ollamaChatModel);
+
+        ChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(15);
+        AiHelperService aiHelperService = AiServices.builder(AiHelperService.class)
+                .chatModel(ollamaChatModel)
+                .streamingChatModel(ollamaStreamingChatModel)
+                .chatMemory(chatMemory)
+                .chatMemoryProvider(memoryId ->
+                        MessageWindowChatMemory.withMaxMessages(15))
+                .build();
+        return aiHelperService;
     }
 }
